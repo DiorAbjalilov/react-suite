5e9e54370cc31443701f7e909cc71516aff23f6d
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-03-09 11:54:03 +0500

    edit add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import DrawerContent from "./components/drawer/DrawerContent";
 import LoaderContent from "./components/loader/LoaderContent";
 import MessageContent from "./components/message/MessageContent";
 import ModalContent from "./components/modal/ModalContent";
+import PlachelhContent from "./components/placheholder/PlachelhContent";
 import ProgresContent from "./components/progress/ProgresContent";
 import TooltipContent from "./components/tooltip/TooltipContent";
 function App() {
@@ -28,6 +29,9 @@ function App() {
       <br />
       <hr />
       <ProgresContent />
+      <br />
+      <hr />
+      <PlachelhContent />
     </>
   );
 }
